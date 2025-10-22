b2ffa2f5a060fca2a05fd595ad0d2968c6fcb94a
Geraldina <geralbm@gmail.com>
2025-10-22 14:02:09 -0300

    Funcion Ingresar Datos

--- back/scripts/notificaciones.js
+++ back/scripts/notificaciones.js
@@ -1,1 +1,51 @@
-alert ("Hola")
+/***************Programa que permite inscribirse al mailing de Turnos **************/
+
+//Desarrollo de Funciones
+
+const validarDatosPac = (nomb, apellido, email, datosOk) => {
+let datosOk = false;
+
+
+
+}
+
+const ingresarDatos = (nomb, apellido, email, datosPacNoti, datosOk) => {
+
+    if (datosOk){
+    datosPacNoti.push(nomb);
+    datosPacNoti.push(apellido);
+    datosPacNoti.push (email);
+    return datosPacNoti;
+    }
+}
+
+//Inicio del Programa Principal
+
+//Inicialización de variables Globales
+const confNoti = false;
+let datosPacNoti = [];
+let nomPac = "";
+let apePac = "";
+let emailPac = "";
+let datosOk = false;
+
+const confNotiTurno = confirm ("¿Desea recibir recordatorio de su tunrno, via email? ");
+
+if (confNotiTurno){
+    nomPac = prompt ("Por favor ingrese su Nombre.");
+    apePac = prompt ("Por favor ingrese su Apellido.");
+    emailPac = prompt ("Por favor, ingrese su e-mail.");
+    //Funciones
+    validarDatosPac (nomPac, apePac, emailPac, datosOk);
+
+    datosPacNoti = ingresarDatos(nomPac, apePac, emailPac, datosPacNoti, datosOk);
+    
+
+    console.log (alert ("Muchas gracias por completar sus datos."));
+    console.log (datosPacNoti);
+}else {
+    console.log (alert ("Muchas gracias por su respuesta."));
+    
+}
+                                                                                            
+
